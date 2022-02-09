b6489d100c7f07aa13ec7b5877598ca8c0987004
VA6DAH <VA6DAH@users.noreply.github.com>
2022-02-09 10:44:07 -0700

    Update Bio.js

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -18,7 +18,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Dakota Hourie`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
@@ -27,10 +27,9 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
-          Francisco building useful things.{' '}
-          <a href="https://twitter.com/kylemathews">
-            You should follow him on Twitter
+          Written by <strong>Dakota</strong> who lives and works in Edmonton, AB and writes about random things.{' '}
+          <a href="www.linkedin.com/in/dakotahourie/">
+            You should add him on Linkedin
           </a>
         </p>
       </div>
