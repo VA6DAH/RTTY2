16d5d71b928e7111d2fca7a25b71d2c4140ff922
VA6DAH <VA6DAH@users.noreply.github.com>
2022-02-08 21:14:36 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'RTTY',
+    author: 'Dakota',
+    description: 'A personal blog with the ramblings of an idiot',
+    siteUrl: 'https://rtty.ca/',
   },
   pathPrefix: '/gatsby-starter-blog',
   plugins: [
